7c8833a07311246a92da3f63eae14a6ffc6ff4e5
Karol Kondracki <karxol@o2.pl>
2022-09-12 21:44:06 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+
+@app.route('/mypage/me', methods=['GET'] )
+def me():
+    return render_template("me.html")
+
+
+@app.route('/mypage/contact',methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == "__main__":
+    app.run()
+
